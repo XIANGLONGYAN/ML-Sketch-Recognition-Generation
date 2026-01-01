b981987f79851a738be587f404e766ac4a95e546
XIANGLONGYAN <2394059386@qq.com>
2026-01-01 22:27:22 +0800

    Initial commit

---
+++ recognition/run.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=2,3,4,5,6 python -u Train.py
+
+
+Move your model into the './pretrain' folder.
+Then, rename the model as 'QD.pkl' (trained on QuickDraw) or 'QD414k.pkl' (trained on QuickDraw414k).
+python -u Eval.py
